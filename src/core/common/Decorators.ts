fc786437400a75015e4089cc30ded02d86456acc
程方 <chengfang@junyougame.com>
2017-05-24 16:16:52 +0800

    增加微软针的懒加载装饰器 方便一些调用后赋值的代码

--- src/core/common/Decorators.ts
+++ src/core/common/Decorators.ts
@@ -25,6 +25,47 @@ module junyou {
     }
 
     /**
+     * 使用微软vs code中使用的代码  
+     * 用于一些 lazy 的调用
+     * https://github.com/Microsoft/vscode/blob/master/src/vs/base/common/decorators.ts
+     * 
+     * @export
+     * @param {*} target 
+     * @param {string} key 
+     * @param {*} descriptor 
+     */
+    export function d_memoize(target: any, key: string, descriptor: any) {
+        let fnKey: string = null;
+        let fn: Function = null;
+
+        if (typeof descriptor.value === 'function') {
+            fnKey = 'value';
+            fn = descriptor.value;
+        } else if (typeof descriptor.get === 'function') {
+            fnKey = 'get';
+            fn = descriptor.get;
+        }
+
+        if (!fn) {
+            throw new Error('not supported');
+        }
+
+        const memoizeKey = `$memoize$${key}`;
+
+        descriptor[fnKey] = function (...args: any[]) {
+            if (!this.hasOwnProperty(memoizeKey)) {
+                Object.defineProperty(this, memoizeKey, {
+                    configurable: false,
+                    enumerable: false,
+                    writable: false,
+                    value: fn.apply(this, args)
+                });
+            }
+            return this[memoizeKey];
+        };
+    }
+
+    /**
      * @private
      */
     const _$l = "__listeners__";
