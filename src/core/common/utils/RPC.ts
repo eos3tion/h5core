06521e22fcacdd881c1e7c476c48aa40c2843025
程方 <chengfang@junyougame.com>
2017-06-28 15:43:44 +0800

    完成RPC的代码

---
+++ src/core/common/utils/RPC.ts
@@ -0,0 +1,170 @@
+module junyou {
+    export interface RPCCallback {
+        /**
+         * 成功的回调函数
+         * 
+         * @type {Recyclable<CallbackInfo<(data?: any, ...args)>>}
+         * @memberof RPCCallback
+         */
+        success: Recyclable<CallbackInfo<{ (data?: any, ...args) }>>;
+
+        /**
+         * 失败的回调函数
+         * 
+         * @type {Recyclable<CallbackInfo<{ (error?: Error, ...args) }>>}
+         * @memberof RPCCallback
+         */
+        error: Recyclable<CallbackInfo<{ (error?: Error, ...args) }>>;
+        /**
+         * RPC的超时时间
+         * 
+         * @type {number}
+         * @memberof RPCCallback
+         */
+        expired: number;
+
+        id: number;
+    }
+
+    export interface RPCInterface {
+
+        /**
+         * 超时的错误常量 `RPCTimeout`
+         * 
+         * @type {string}
+         * @memberof RPCInterface
+         */
+        readonly Timeout: string;
+        /**
+         * 执行回调
+         * 
+         * @param {number} id 执行回调的id
+         * @param {*} [data] 成功返回的数据
+         * @param {(Error | string)} [err] 错误
+         */
+        callback(id: number, data?: any, err?: Error | string);
+        /**
+         * 注册回调函数
+         * 
+         * @param {Recyclable<CallbackInfo<{ (data?: any, ...args) }>>} success     成功的函数回调
+         * @param {Recyclable<CallbackInfo<{ (error?: Error, ...args) }>>} [error]    发生错误的函数回调
+         * @param {number} [timeout=2000] 超时时间，默认2000，实际超时时间会大于此时间，超时后，如果有错误回调，会执行错误回调，`Error(RPC.Timeout)`
+         * @returns 
+         */
+        registerCallback(success: Recyclable<CallbackInfo<{ (data?: any, ...args) }>>, error?: Recyclable<CallbackInfo<{ (error?: Error, ...args) }>>, timeout?: number)
+        /**
+         * 根据id移除回调函数
+         * 
+         * @param {number} id 
+         */
+        removeCallback(id: number)
+    }
+
+    export const RPC: RPCInterface = (function () {
+        let seed = 0;
+        let callbacks = {} as { [index: number]: RPCCallback };
+        const Timeout = "RPCTimeout";
+        let count = 0;
+        let start: boolean;
+        let willDel = [];
+        return {
+            Timeout,
+            /**
+             * 执行回调
+             * 
+             * @param {number} id 执行回调的id
+             * @param {*} [data] 成功返回的数据
+             * @param {(Error | string)} [err] 错误
+             */
+            callback(id: number, data?: any, err?: Error | string) {
+                let callback = callbacks[id];
+                if (!callback) {
+                    return
+                }
+                deleteCallback(id);
+                let { success, error } = callback;
+                if (err) {
+                    if (typeof err === "string") {
+                        err = new Error(err);
+                    }
+                    if (error) {
+                        error.call(err);
+                        error.recycle();
+                    }
+                    if (success) {
+                        success.recycle();
+                    }
+                } else {
+                    if (error) {
+                        error.recycle();
+                    }
+                    if (success) {
+                        success.call(data);
+                        success.execute();
+                    }
+                }
+            },
+            /**
+             * 注册回调函数
+             * 
+             * @param {Recyclable<CallbackInfo<{ (data?: any, ...args) }>>} success     成功的函数回调
+             * @param {Recyclable<CallbackInfo<{ (error?: Error, ...args) }>>} [error]    发生错误的函数回调
+             * @param {number} [timeout=2000] 超时时间，默认2000，实际超时时间会大于此时间，超时后，如果有错误回调，会执行错误回调，`Error(RPC.Timeout)`
+             * @returns 
+             */
+            registerCallback(success: Recyclable<CallbackInfo<{ (data?: any, ...args) }>>, error?: Recyclable<CallbackInfo<{ (error?: Error, ...args) }>>, timeout = 2000) {
+                let id = seed++;
+                callbacks[id] = { id, expired: Global.now + timeout, success, error };
+                count++;
+                if (!start) {
+                    TimerUtil.addCallback(Time.ONE_SECOND, check);
+                    start = true;
+                }
+                return id;
+            },
+            /**
+             * 根据id移除回调函数
+             * 
+             * @param {number} id 
+             */
+            removeCallback(id: number) {
+                let callback = callbacks[id];
+                deleteCallback(id);
+                if (callback) {
+                    let { success, error } = callback;
+                    if (success) {
+                        success.recycle();
+                    }
+                    if (error) {
+                        error.recycle();
+                    }
+                }
+            }
+        }
+        function deleteCallback(id: number) {
+            if (id in callbacks) {
+                delete callbacks[id];
+                count--;
+                if (count == 0) {
+                    TimerUtil.removeCallback(Time.ONE_SECOND, check);
+                    start = false;
+                }
+            }
+        }
+        function check() {
+            let del = willDel;
+            let i = 0;
+            let now = Global.now;
+            for (let id in callbacks) {
+                let callback = callbacks[id];
+                if (now > callback.expired) {
+                    del[i++] = id;
+                }
+            }
+            for (let j = 0; j < i; j++) {
+                let id = del[j];
+                deleteCallback(id);
+            }
+        }
+    })()
+}
