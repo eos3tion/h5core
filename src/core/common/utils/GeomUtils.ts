f4bd348b94657e3b77a126716b689678945d7b4b
程方 <chengfang@junyougame.com>
2017-07-13 11:30:24 +0800

    增加面积计算公式的代码

--- src/core/common/utils/GeomUtils.ts
+++ src/core/common/utils/GeomUtils.ts
@@ -4,12 +4,13 @@ module junyou {
      * 获取多个点的几何中心点
      * 
      * @export
-     * @param {{ x: number, y: number }[]} points 点集
-     * @returns 点集的几何中心点
+     * @param {Point[]} points 点集
+     * @param {Point} result 结果
+     * @returns {Point} 点集的几何中心点
      * @author gushuai
      */
-    export function getCenter(points: { x: number, y: number }[], result?: { x: number, y: number }) {
-        result = result || {} as { x: number, y: number };
+    export function getCenter(points: Point[], result?: Point) {
+        result = result || {} as Point;
         let len = points.length;
         let x = 0;
         let y = 0;
@@ -26,18 +27,38 @@ module junyou {
     /**
      * 检查类矩形 a 和 b 是否相交
      * @export
-     * @param {{ x: number, y: number, width: number, height: number }} a   类矩形a
-     * @param {{ x: number, y: number, width: number, height: number }} b   类矩形b
-     * @return true     表示两个类似矩形的物体相交
+     * @param {Rect} a   类矩形a
+     * @param {Rect} b   类矩形b
+     * @returns {boolean} true     表示两个类似矩形的物体相交
      *         false    表示两个类似矩形的物体不相交
      */
-    export function intersects(a: { x: number, y: number, width: number, height: number }, b: { x: number, y: number, width: number, height: number }): boolean {
+    export function intersects(a: Rect, b: Rect): boolean {
         let aright = a.x + a.width;
         let abottom = a.y + a.height;
         let bright = b.x + b.width;
         let bbottom = b.y + b.height;
         return Math.max(a.x, b.x) <= Math.min(aright, bright)
             && Math.max(a.y, b.y) <= Math.min(abottom, bbottom);
+    }
+
+    /**
+     * 获取点集围成的区域的面积
+     * S=（（X2-X1）*  (Y2+Y1)+（X2-X2）*  (Y3+Y2)+（X4-X3）*  (Y4+Y3)+……+（Xn-Xn-1）*  (Yn+Yn-1)+（X1-Xn）*  (Y1+Yn)）/2
+     * @export
+     * @param {Point[]} points 点集
+     * @returns {number}
+     */
+    export function getArea(points: Point[]) {
+        let p0 = points[0];
+        let s = 0;
+        let last = p0;
+        for (let i = 1; i < length; i++) {
+            let p = points[i];
+            s += (p.x - last.x) * (p.y + last.y);
+            last = p;
+        }
+        s += (p0.x - last.x) * (p0.y + last.y);
+        return s * .5;
     }
 
 }
