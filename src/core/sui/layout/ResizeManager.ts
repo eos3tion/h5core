b0be8c7fb5bcbeb35559d461d677dcd0c3c9c85b
程方 <chengfang@junyougame.com>
2017-09-13 11:40:51 +0800

    增加Resize管理器，以供后续arpg项目使用

---
+++ src/core/sui/layout/ResizeManager.ts
@@ -0,0 +1,84 @@
+module junyou {
+
+    export interface ResizeBin {
+        layout: LayoutType;
+        hoffset?: number;
+        voffset?: number;
+        container?: egret.DisplayObjectContainer;
+    }
+
+    export declare type ResizeDisplay = egret.DisplayObject & { $_resize?: ResizeBin };
+    export class ResizeManager {
+        protected _stage: egret.Stage;
+
+        protected _list: ResizeDisplay[] = [];
+
+
+        public init(stage?: egret.Stage) {
+            this._stage = stage || egret.sys.$TempStage;
+            this._stage.on(egret.Event.RESIZE, this.onResize, this);
+        }
+
+
+        /**
+         * 
+         * 添加一个元件，相对于container的布局
+         * @param {ResizeDisplay} dis 
+         * @param {number} layout 
+         * @param {number} [hoffset] 
+         * @param {number} [voffset] 
+         * @param {egret.DisplayObjectContainer} [container] 默认使用stage
+         * 
+         * @memberOf ResizeManager
+         */
+        public add(dis: ResizeDisplay, layout: number, hoffset?: number, voffset?: number, container?: egret.DisplayObjectContainer) {
+            const list = this._list;
+            let len = list.length;
+            dis.$_resize = { layout, hoffset, voffset, container };
+            list[len] = dis;
+            if (!dis.stage) {
+                dis.on(egret.Event.ADDED_TO_STAGE, this.onAdded, this);
+            }
+        }
+
+        /**
+         * 移除元件
+         * 
+         * @param {egret.DisplayObject} dis (description)
+         * @returns (description)
+         */
+        public remove(dis: ResizeDisplay) {
+            this._list.remove(dis);
+            dis.off(egret.Event.ADDED_TO_STAGE, this.onAdded, this);
+            if (dis.$_resize) {
+                dis.$_resize = undefined;
+            }
+        }
+
+        protected onAdded(e: egret.Event) {
+            let dis = e.target as ResizeDisplay;
+            let index = this._list.indexOf(dis);
+            if (index < 0) return;
+            this.resize(dis);
+        }
+
+        protected resize(dis: ResizeDisplay) {
+            let info = dis.$_resize;
+            let container = info.container || egret.sys.$TempStage;
+            Layout.layout(dis, info.layout, info.hoffset, info.voffset, true, true, container);
+        }
+
+        protected onResize() {
+            const list = this._list;
+            let len = list.length;
+            for (let i = 0; i < len; i++) {
+                this.resize(list[i]);
+            }
+        }
+
+        public dispose() {
+            this._stage.off(egret.Event.ADDED_TO_STAGE, this.onAdded, this);
+            this._list.length = 0;
+        }
+    }
+}
