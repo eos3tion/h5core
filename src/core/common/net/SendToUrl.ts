a35fc235e72240de5a080970505b9a357aec28e1
程方 <chengfang@junyougame.com>
2017-09-13 15:00:38 +0800

    使用Image，在https下，不会因为最终请求地址为http，导致浏览器将请求拦截，详情参考 https://developer.mozilla.org/en-US/docs/Web/Security/Mixed_content

--- src/core/common/net/SendToUrl.ts
+++ src/core/common/net/SendToUrl.ts
@@ -1,52 +1,45 @@
 module junyou {
-
     /**
-     * 
-     * 发起可以不需要回调响应的跨域get请求
-     * @param {string} url          发起请求的地址
-     * @param {boolean} [always]    是否总是发起请求  
-     *                              false（默认） 请求已经在列队中，则不会重复发起请求  
-     *                              true 不管相同地址的请求之前是否已经发起，继续发起请求
-     */
-    export const sendToUrl = (function () {
-        const _unSendList = [];
-        const req = new egret.HttpRequest;
-        req.on(egret.Event.COMPLETE, callBack(RequestState.COMPLETE));
-        req.on(egret.IOErrorEvent.IO_ERROR, callBack(RequestState.FAILED));
-        let _requestState = RequestState.UNREQUEST;
-        if (DEBUG) {
-            var _url: string;
-        }
-
-        return function (url: string, always?: boolean) {
-            if (_requestState == RequestState.REQUESTING) {
-                if (always) {
-                    _unSendList.push(url);
-                } else {
-                    _unSendList.pushOnce(url);
-                }
-                return;
-            }
-            _requestState = RequestState.REQUESTING;
-            req.open(url, "GET");
-            req.send();
-            if (DEBUG) {
-                _url = url;
-                console.log(_url, "send");
-            }
-        };
-
-        function callBack(state: RequestState) {
-            return () => {
-                _requestState = state;
-                if (DEBUG) {
-                    console.log(_url, "callBack:", state);
-                }
-                if (_unSendList.length > 0) {
-                    return sendToUrl(_unSendList.shift());
-                }
-            }
-        }
-    })();
-
+    * 
+    * 发起可以不需要回调响应的跨域get请求
+    * @param {string} url          发起请求的地址
+    * @param {boolean} [always]    是否总是发起请求  
+    *                              false（默认） 请求已经在列队中，则不会重复发起请求  
+    *                              true 不管相同地址的请求之前是否已经发起，继续发起请求
+    */
+    export const sendToUrl = $();
 }
+
+function $() {
+    const _unSendList = [];
+    const img = new Image();//使用Image，在https下，不会因为最终请求地址为http，导致浏览器将请求拦截，详情参考 https://developer.mozilla.org/en-US/docs/Web/Security/Mixed_content
+    let _requestState = junyou.RequestState.UNREQUEST;
+    img.onerror = callBack(this, junyou.RequestState.FAILED);
+    img.onload = callBack(this, junyou.RequestState.COMPLETE);
+    return sendToUrl;
+    function sendToUrl(url: string, always?: boolean) {
+        if (_requestState == junyou.RequestState.REQUESTING) {
+            if (always) {
+                _unSendList.push(url);
+            } else {
+                _unSendList.pushOnce(url);
+            }
+            return;
+        }
+        _requestState = junyou.RequestState.REQUESTING;
+        img.src = url;
+    };
+
+    function callBack(self, state: junyou.RequestState) {
+        return () => {
+            _requestState = state;
+            if (DEBUG) {
+                console.log(img.src, "callBack:", state);
+            }
+            if (_unSendList.length > 0) {
+                return sendToUrl(_unSendList.shift());
+            }
+        }
+    }
+}
+
