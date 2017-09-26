ea331eb5cc7bdbda327e3888f76eeb596cd3ec7e
程方 <chengfang@junyougame.com>
2017-09-26 15:16:15 +0800

    生成对应脚本和定义

--- bin/h5core/h5core.js
+++ bin/h5core/h5core.js
@@ -14435,8 +14435,11 @@ var junyou;
     var inter1 = "addReadyExecute";
     var inter2 = "startSync";
     function isIAsync(instance) {
+        if (!instance) {
+            return false;
+        }
         /*不验证数据，因为现在做的是get asyncHelper(),不默认创建AsyncHelper，调用的时候才创建，这样会导致多一次调用*/
-        if (!(inter1 in instance || typeof instance[inter2] !== "function") /*|| !(instance[inter1] instanceof AsyncHelper)*/) {
+        if (!(inter1 in instance || typeof instance[inter1] !== "function") /*|| !(instance[inter1] instanceof AsyncHelper)*/) {
             return false;
         }
         if (!(inter2 in instance) || typeof instance[inter2] !== "function" || instance[inter2].length != 0) {
