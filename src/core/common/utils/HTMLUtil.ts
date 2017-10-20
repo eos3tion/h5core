3903151a552f764234745cd573ab8591f8f4e550
程方 <chengfang@junyougame.com>
2017-10-20 18:52:55 +0800

    优化写法

--- src/core/common/utils/HTMLUtil.ts
+++ src/core/common/utils/HTMLUtil.ts
@@ -3,59 +3,62 @@ module junyou {
      * HTML工具类
      * @author 3tion
      */
-    export const HTMLUtil = (function () {
-        const unescChars = { "&lt;": "<", "&gt;": ">", "&quot;": "\"", "&apos;": "\'", "&amp;": "&", "&nbsp;": " ", "&#x000A;": "\n" };
-        const escChars = { "<": "&lt;", ">": "&gt;", "'": "&apos;", "\"": "&quot;", "&": "&amp;" };
-        return {
-            /**
-             * 字符着色
-             * 
-             * @param {string | number} value                内容
-             * @param {(string | number)} color     颜色
-             * @returns 
-             */
-            createColorHtml(value: string | number, color: string | number) {
-                let c: string;
-                if (typeof color == "number") {
-                    c = ColorUtil.getColorString(color);
-                } else if (color.charAt(0) != "#") {
-                    c = "#" + color;
-                } else {
-                    c = color;
-                }
-                return "<font color=\'" + c + "\'>" + value + "</font>";
-            },
+    const unescChars = { "&lt;": "<", "&gt;": ">", "&quot;": "\"", "&apos;": "\'", "&amp;": "&", "&nbsp;": " ", "&#x000A;": "\n" };
+    const escChars = { "<": "&lt;", ">": "&gt;", "'": "&apos;", "\"": "&quot;", "&": "&amp;" };
 
-            /**
-             * 清理html;
-             * @value value
-             * @return
-             *
-             */
-            clearHtml(value: string) {
-                return value.replace(/<[^><]*?>/g, "");
-            },
-            /**
-             * 将特殊字符串处理为HTML转义字符
-             * 
-             * @param {string} content 
-             */
-            escapeHTML(content: string) {
-                return content.replace(/<|>|"|'|&/g, substring => {
-                    return escChars[substring];
-                });
-            },
-            /**
-             * 将HTML特殊符号，恢复成正常字符串
-             * 
-             * @param {string} content 
-             * @returns 
-             */
-            unescapeHTML(content: string) {
-                return content.replace(/&lt;|&gt;|&quot;|&apos;|&amp;|&nbsp;|&#x000A;/g, substring => {
-                    return unescChars[substring];
-                });
+    function escFun(substring: string) {
+        return escChars[substring];
+    }
+
+    function unescFun(substring: string) {
+        return unescChars[substring];
+    }
+
+    export const HTMLUtil = {
+        /**
+         * 字符着色
+         * 
+         * @param {string | number} value       内容
+         * @param {(string | number)} color     颜色
+         * @returns 
+         */
+        createColorHtml(value: string | number, color: string | number) {
+            let c: string;
+            if (typeof color == "number") {
+                c = ColorUtil.getColorString(color);
+            } else if (color.charAt(0) != "#") {
+                c = "#" + color;
+            } else {
+                c = color;
             }
+            return "<font color=\'" + c + "\'>" + value + "</font>";
+        },
+
+        /**
+         * 清理html;
+         * @value value
+         * @return
+         *
+         */
+        clearHtml(value: string) {
+            return value.replace(/<[^><]*?>/g, "");
+        },
+        /**
+         * 将特殊字符串处理为HTML转义字符
+         * 
+         * @param {string} content 
+         */
+        escHTML(content: string) {
+            return content.replace(/<|>|"|'|&/g, escFun);
+        },
+        /**
+         * 将HTML特殊符号，恢复成正常字符串
+         * 
+         * @param {string} content 
+         * @returns 
+         */
+        unescHTML(content: string) {
+            return content.replace(/&lt;|&gt;|&quot;|&apos;|&amp;|&nbsp;|&#x000A;/g, unescFun);
         }
-    })();
+    }
 }
--- src/core/common/utils/TimerUtil.ts
+++ src/core/common/utils/TimerUtil.ts
@@ -15,95 +15,94 @@ module junyou {
         nt: number;
 
     }
-    export const TimerUtil = (function () {
-        const _timeobj: { [index: number]: GTimer } = {};
-        let tmpList: CallbackInfo<Function>[] = [];
-        let willDeleted: string[] = [];
-        return { addCallback, removeCallback, tick };
-        function tick(now: number) {
-            let d = 0;
-            for (let key in _timeobj) {
-                let timer = _timeobj[key];
-                if (timer.nt < now) {
-                    timer.nt = now + timer.tid;
-                    let list = timer.list;
-                    let len = list.length;
-                    if (len > 0) {
-                        for (let i = 0; i < len; i++) {
-                            tmpList[i] = list[i];
-                        }
-                        for (let i = 0; i < len; i++) {
-                            tmpList[i].execute(false);
-                        }
-                    }
-                    len = list.length;
-                    if (len == 0) {
-                        willDeleted[d++] = key;
-                    }
-                }
-            }
-            for (let i = 0; i < d; i++) {
-                delete _timeobj[willDeleted[i]];
-            }
-        }
-
-        function getInterval(time: number) {
-            return Math.ceil(time / 10) * 10;
-        }
-        /**
-         * 
-         * 注册回调
-         * @static
-         * @param {number} time 回调的间隔时间，间隔时间会处理成30的倍数，向上取整，如 设置1ms，实际间隔为30ms，32ms，实际间隔会使用60ms
-         * @param {Function} callback 回调函数，没有加this指针是因为做移除回调的操作会比较繁琐，如果函数中需要使用this，请通过箭头表达式()=>{}，或者将this放arg中传入
-         * @param {any} [thisObj] 回调函数的`this`对象，不传值则使用全局上下文即window
-         * @param {any} args 回调函数的参数
-         */
-        function addCallback(time: number, callback: Function, thisObj?: any, ...args) {
-            time = getInterval(time);
-            let timer = _timeobj[time];
-            if (!timer) {
-                timer = <GTimer>{};
-                timer.tid = time;//setInterval(check, time, timer);
-                timer.nt = Global.now + time;
-                let list: CallbackInfo<Function>[] = [];
-                timer.list = list;
-                _timeobj[time] = timer;
-                list.push(CallbackInfo.get(callback, thisObj, ...args));
-            } else {
-                CallbackInfo.addToList(timer.list, callback, thisObj, ...args);
-            }
-        }
-
-        /**
-         * 移除回调
-         * 
-         * @static
-         * @param {number} time         回调的间隔时间，间隔时间会处理成30的倍数，向上取整，如 设置1ms，实际间隔为30ms，32ms，实际间隔会使用60ms
-         * @param {Function} callback   回调函数，没有加this指针是因为做移除回调的操作会比较繁琐，如果函数中需要使用this，请通过箭头表达式()=>{}，或者将this放arg中传入
-         * @param {*} [thisObj]         回调函数的`this`对象
-         */
-        function removeCallback(time: number, callback: Function, thisObj?: any) {
-            time = getInterval(time);
-            let timer = _timeobj[time];
-            if (timer) {
+    const _timeobj: { [index: number]: GTimer } = {};
+    let tmpList: CallbackInfo<Function>[] = [];
+    let willDeleted: string[] = [];
+    function tick(now: number) {
+        let d = 0;
+        for (let key in _timeobj) {
+            let timer = _timeobj[key];
+            if (timer.nt < now) {
+                timer.nt = now + timer.tid;
                 let list = timer.list;
-                let j = -1;
-                for (let i = 0, len = list.length; i < len; i++) {
-                    let info = list[i];
-                    if (info.checkHandle(callback, thisObj)) {
-                        j = i;
-                        break;
+                let len = list.length;
+                if (len > 0) {
+                    for (let i = 0; i < len; i++) {
+                        tmpList[i] = list[i];
+                    }
+                    for (let i = 0; i < len; i++) {
+                        tmpList[i].execute(false);
                     }
                 }
-                if (~j) {
-                    list.splice(j, 1);
-                }
-                if (!list.length) {
-                    clearInterval(timer.tid);
-                    delete _timeobj[time];
+                len = list.length;
+                if (len == 0) {
+                    willDeleted[d++] = key;
                 }
             }
         }
-    })();
+        for (let i = 0; i < d; i++) {
+            delete _timeobj[willDeleted[i]];
+        }
+    }
+
+    function getInterval(time: number) {
+        return Math.ceil(time / 10) * 10;
+    }
+    /**
+     * 
+     * 注册回调
+     * @static
+     * @param {number} time 回调的间隔时间，间隔时间会处理成30的倍数，向上取整，如 设置1ms，实际间隔为30ms，32ms，实际间隔会使用60ms
+     * @param {Function} callback 回调函数，没有加this指针是因为做移除回调的操作会比较繁琐，如果函数中需要使用this，请通过箭头表达式()=>{}，或者将this放arg中传入
+     * @param {any} [thisObj] 回调函数的`this`对象，不传值则使用全局上下文即window
+     * @param {any} args 回调函数的参数
+     */
+    function addCallback(time: number, callback: Function, thisObj?: any, ...args) {
+        time = getInterval(time);
+        let timer = _timeobj[time];
+        if (!timer) {
+            timer = <GTimer>{};
+            timer.tid = time;//setInterval(check, time, timer);
+            timer.nt = Global.now + time;
+            let list: CallbackInfo<Function>[] = [];
+            timer.list = list;
+            _timeobj[time] = timer;
+            list.push(CallbackInfo.get(callback, thisObj, ...args));
+        } else {
+            CallbackInfo.addToList(timer.list, callback, thisObj, ...args);
+        }
+    }
+
+    /**
+     * 移除回调
+     * 
+     * @static
+     * @param {number} time         回调的间隔时间，间隔时间会处理成30的倍数，向上取整，如 设置1ms，实际间隔为30ms，32ms，实际间隔会使用60ms
+     * @param {Function} callback   回调函数，没有加this指针是因为做移除回调的操作会比较繁琐，如果函数中需要使用this，请通过箭头表达式()=>{}，或者将this放arg中传入
+     * @param {*} [thisObj]         回调函数的`this`对象
+     */
+    function removeCallback(time: number, callback: Function, thisObj?: any) {
+        time = getInterval(time);
+        let timer = _timeobj[time];
+        if (timer) {
+            let list = timer.list;
+            let j = -1;
+            for (let i = 0, len = list.length; i < len; i++) {
+                let info = list[i];
+                if (info.checkHandle(callback, thisObj)) {
+                    j = i;
+                    break;
+                }
+            }
+            if (~j) {
+                list.splice(j, 1);
+            }
+            if (!list.length) {
+                clearInterval(timer.tid);
+                delete _timeobj[time];
+            }
+        }
+    }
+
+    export const TimerUtil = { addCallback, removeCallback, tick };
 }
