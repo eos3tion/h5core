471e0ac3cb8a4c0c74bf803a8a90febda9426b4f
程方 <chengfang@junyougame.com>
2017-10-21 15:50:18 +0800

    生成定义

--- bin/h5core/h5core.d.ts
+++ bin/h5core/h5core.d.ts
@@ -257,7 +257,7 @@ interface Array<T> {
 }
 declare module junyou {
     function is(instance: any, ref: {
-        new (): any;
+        new(): any;
     }): boolean;
     /**
      * 移除可视对象
@@ -420,7 +420,7 @@ interface $gmType {
 }
 declare module junyou {
     type InjectProxy = {
-        new (): IAsync;
+        new(): IAsync;
     } | string | number;
     /**
      * Mediator和Proxy的基类
@@ -493,7 +493,7 @@ declare module junyou {
      * @returns
      */
     function __dependProxy(ref: {
-        new (): IAsync;
+        new(): IAsync;
     } | string | number): (target: any, key: string) => void;
 }
 declare module junyou {
@@ -1725,7 +1725,7 @@ declare module junyou {
          * @memberOf MixinOption
          */
         clazz: {
-            new (): T;
+            new(): T;
         };
         /**
          *
@@ -3583,7 +3583,7 @@ declare module junyou {
          * @memberOf PBStructDict
          */
         $$inted?: any;
-        /**消息名称*/ [index: string]: PBStruct;
+        /**消息名称*/[index: string]: PBStruct;
     }
     /**
      *
@@ -4591,6 +4591,20 @@ declare module junyou {
         Green = 65280,
         Yellow = 16776960,
         White = 16777215,
+        Gray = 13421772,
+    }
+    /**
+     * 常用的颜色字符串常量
+     *
+     * @export
+     * @enum {string}
+     */
+    const enum ColorString {
+        Red = "#ff0000",
+        Green = "#00ff00",
+        Yellow = "#ffff00",
+        White = "#ffffff",
+        Gray = "#cccccc",
     }
     /**
      * 颜色工具
@@ -5071,7 +5085,7 @@ declare module junyou {
         };
         static instance: GameEngine;
         static init(stage: egret.Stage, ref?: {
-            new (stage: egret.Stage): GameEngine;
+            new(stage: egret.Stage): GameEngine;
         }): void;
         static addLayerConfig(id: number, parentid?: number, ref?: new (id: number) => GameLayer): void;
         /**
@@ -6513,7 +6527,7 @@ declare module junyou {
         pathdata?: Uint8Array;
         constructor();
         static decodeFromArray(arr: any[], ref?: {
-            new (): MapInfo;
+            new(): MapInfo;
         }): MapInfo;
         /**
         * 获取资源路径
@@ -6755,8 +6769,8 @@ declare module junyou {
         getPath(fx: number, fy: number, tx: number, ty: number, callback: CallbackInfo<{
             (path: PathNode[], ...args);
         }>): {
-            stop: boolean;
-        };
+                stop: boolean;
+            };
     }
 }
 declare module junyou {
@@ -8169,7 +8183,7 @@ declare module junyou {
          * @memberOf Facade
          */
         static getNameOfInline(inlineRef: {
-            new (): any;
+            new(): any;
         }, className?: string): string;
         /**
          * 存储的数据Proxy
@@ -8226,7 +8240,7 @@ declare module junyou {
          * @param {boolean} [async=false] 是否异步初始化，默认直接初始化
          */
         registerInlineProxy(ref: {
-            new (): Proxy;
+            new(): Proxy;
         }, proxyName?: Key, async?: boolean): void;
         /**
          *
@@ -8235,7 +8249,7 @@ declare module junyou {
          * @param {string} [mediatorName]   注册的模块名字
          */
         registerInlineMediator(ref: {
-            new (): Mediator;
+            new(): Mediator;
         }, mediatorName?: Key): void;
         /**
          * 注册Proxy的配置
@@ -8678,9 +8692,9 @@ declare module junyou {
      *
      */
     var DataLocator: {
-        regParser: (key: "ani", parser: ConfigDataParser) => void;
+        regParser: (key: keyof CfgData, parser: ConfigDataParser) => void;
         parsePakedDatas(): void;
-        regCommonParser(key: "ani", CfgCreator: 0 | (new () => Cfg), idkey?: string): void;
+        regCommonParser(key: keyof CfgData, CfgCreator: 0 | (new () => Cfg), idkey?: string): void;
     };
     /**
      * 配置数据解析函数
@@ -9990,10 +10004,10 @@ declare module junyou {
      * 创建器
      */
     type Creator<T> = {
-        new (): T;
+        new(): T;
     } | {
-        (): T;
-    };
+            (): T;
+        };
     /**
      *
      * 调整ClassFactory
@@ -10070,7 +10084,7 @@ declare module junyou {
      * @param {({ new(): T & { _pool?: RecyclablePool<T> } })} clazz
      */
     function recyclable<T>(clazz: {
-        new (): T & {
+        new(): T & {
             _pool?: RecyclablePool<T>;
         };
     }): any;
@@ -10092,7 +10106,7 @@ declare module junyou {
      * @param clazz 要做单例的类型
      */
     function singleton<T>(clazz: {
-        new (): T;
+        new(): T;
         _instance?: T;
     }): T;
 }
@@ -10526,7 +10540,7 @@ declare module junyou {
          */
         protected _creators: {
             [index: string]: {
-                new (): BaseCreator<egret.DisplayObject>;
+                new(): BaseCreator<egret.DisplayObject>;
             };
         };
         /**
@@ -10897,7 +10911,7 @@ interface Window {
     XMLHttpRequest?: XMLHttpRequest;
 }
 interface ActiveXObject {
-    new (key: "MSXML2.XMLHTTP"): XMLHttpRequest;
+    new(key: "MSXML2.XMLHTTP"): XMLHttpRequest;
 }
 declare const ActiveXObject: ActiveXObject;
 declare module junyou {
@@ -11924,7 +11938,7 @@ declare module junyou {
          * @type {{new():T}}
          */
         renderClass: {
-            new (): T;
+            new(): T;
         };
         /**
          * 背景
