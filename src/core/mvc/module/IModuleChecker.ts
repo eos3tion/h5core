70fb1244d0ceef1a7a69814ad9718b53cd87d78f
程方 <chengfang@junyougame.com>
2017-12-25 16:59:50 +0800

    不再强制要求limitChecker的数据必须是数组

--- src/core/mvc/module/IModuleChecker.ts
+++ src/core/mvc/module/IModuleChecker.ts
@@ -5,14 +5,14 @@ module junyou {
 	 *
 	 */
 	export interface IModuleChecker extends ILimitChecker {
-    	
+
     	/**
 		 * 检查并修正显示限制和使用限制值配错的情况
-		 * @param	{any[]}	showLimits		显示限制的数据
-		 * @param	{any[]}	limits			使用限制的数据
+		 * @param	{any}	showLimits		显示限制的数据
+		 * @param	{any}	limits			使用限制的数据
 		 * @return	{boolean}   <br/>true 有配置错误<br/>false 无配置错误
-		 */		
-        adjustLimitDatas(showLimits: any[],limits: any[]): boolean;
+		 */
+		adjustLimitDatas(showLimits: any, limits: any): boolean;
 
 	}
 }
