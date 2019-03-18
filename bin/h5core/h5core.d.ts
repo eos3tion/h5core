c8d4e160029b18ad3c4f7774866166f8cd0b11fa
程方 <chengfang@tpulse-tech.com>
2019-03-18 14:20:01 +0800

    调整文件名和定义文件

--- bin/h5core/h5core.d.ts
+++ bin/h5core/h5core.d.ts
@@ -8680,6 +8680,17 @@ declare namespace jy {
         checkWord: typeof checkWord1;
     };
 }
+/**
+ * 是否不做客户端检查
+ * 客户端检查的部分，后续统一按下面例子处理
+ * @example
+ *  if (!noClientCheck) {
+ *       if (!$hero.clan) {
+ *          return CoreFunction.showClientTips(MsgCodeConst.Code_883);
+ *      }
+ *  }
+ */
+declare var noClientCheck: boolean;
 declare namespace jy {
     /**
      * 错误前缀
