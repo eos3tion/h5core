db0080c9056ecaf1ccf27af943a3835976a3c278
程方 <eos_3tion@163.com>
2020-09-16 17:47:32 +0800

    优化写法，现在打包的纹理尺寸会单独存放 避免因为binPacker加了1px padding，导致尺寸放大变模糊的问题

--- bin/h5core/h5core.d.ts
+++ bin/h5core/h5core.d.ts
@@ -1141,7 +1141,7 @@ declare namespace jy {
         /**
          * 更新纹理
          */
-        update: (key: string | number, { x, y, width, height }: Rect, tex: egret.Texture) => void;
+        update: (key: string | number, rect: Rect, tex: DynamicTexture) => void;
         /**
          * 删除指定纹理
          * @param key
@@ -1166,6 +1166,11 @@ declare namespace jy {
          */
         getSize(): number;
     };
+    interface DynamicTexture extends egret.Texture {
+        $bin?: Bin;
+        sheet?: TextureSheet;
+        $rect?: Rect;
+    }
     type TextureSheet = ReturnType<typeof getTextureSheet>;
 }
 declare namespace jy {
@@ -2325,7 +2330,7 @@ declare namespace jy {
     export {};
 }
 declare namespace jy {
-    export function getDynamicTexSheet(size?: number): {
+    function getDynamicTexSheet(size?: number): {
         bind: (uri: string | number, tex: DynamicTexture) => any;
         draw: (uri: string | number, display: egret.DisplayObject, clipBounds?: egret.Rectangle, scale?: number) => DynamicTexture;
         update: (uri: string | number, tex: egret.Texture) => void;
@@ -2333,12 +2338,7 @@ declare namespace jy {
         remove(uri: string | number): void;
         get: (uri: string | number) => egret.Texture;
     };
-    interface DynamicTexture extends egret.Texture {
-        $bin?: Bin;
-        sheet?: TextureSheet;
-    }
-    export type DynamicTexSheet = ReturnType<typeof getDynamicTexSheet>;
-    export {};
+    type DynamicTexSheet = ReturnType<typeof getDynamicTexSheet>;
 }
 declare namespace jy {
     /**
