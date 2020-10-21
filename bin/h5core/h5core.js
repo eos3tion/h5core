4598cadf567deb781673d6eaae24304c177e3313
程方 <eos_3tion@163.com>
2020-10-21 16:14:09 +0800

    有sheet的不销毁，只切断引用

--- bin/h5core/h5core.js
+++ bin/h5core/h5core.js
@@ -8726,7 +8726,9 @@ var jy;
                         sheet.remove(this.uri);
                     }
                 }
-                tex.dispose();
+                if (!tex.sheet) { //有sheet的不销毁，只切断引用
+                    tex.dispose();
+                }
             }
             this._list.length = 0;
             this.state = 0 /* UNREQUEST */;
