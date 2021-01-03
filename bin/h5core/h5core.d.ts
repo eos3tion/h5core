c80d0a192b6ed8b3dd5c3b6411bc706860aa0475
程方 <eos_3tion@163.com>
2021-01-03 10:59:20 +0800

    生成tiledMapPB

--- bin/h5core/h5core.d.ts
+++ bin/h5core/h5core.d.ts
@@ -10773,7 +10773,8 @@ declare namespace jy {
         NavMeshMapInfoPB = 107,
         PointGroupPB = 108,
         PolyPointIdxPB = 109,
-        TPointIdxPB = 110
+        TPointIdxPB = 110,
+        TiledMapPB = 111
     }
     /**
      * 地图的PB数据
@@ -10935,6 +10936,10 @@ declare namespace jy {
          * 可选参数 没有地图数据的数据索引
          */
         noPic?: ByteArray;
+        /**
+         * 可选参数 tiled生成的地图数据
+         */
+        tiledMap?: TiledMapPB;
     }
 }
 /**
@@ -11087,6 +11092,34 @@ declare namespace jy {
          * 点C索引
          */
         c: number;
+    }
+}
+/**
+ * 使用JunyouProtoTools，从 [文本框中，复制粘贴] 生成
+ * 生成时间 2020-12-31 15:50:29
+ **/
+declare namespace jy {
+    interface TiledMapPB {
+        /**
+         * 总列数
+         */
+        cols: number;
+        /**
+         * 总行数
+         */
+        rows: number;
+        /**
+         *
+         */
+        tileWidth: number;
+        /**
+         *
+         */
+        tileHeight: number;
+        /**
+         * 可选参数 每一层的tile的id的字节数组，一个字节为一个tile的id
+         */
+        layers?: ByteArray[];
     }
 }
 declare namespace jy {
